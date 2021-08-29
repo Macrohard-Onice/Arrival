e7d7832b579bdb31c705309c98873e770ff21209
Macrohard-Onice <hyvhenthefox@gmail.com>
2021-08-29 23:11:10 +0200

    switch html

--- android/app/src/main/kotlin/com/example/arrival_main/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.arrival_main
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/autogen/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.autogen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
